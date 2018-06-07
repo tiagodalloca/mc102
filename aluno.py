7b03b7304c84b61ae356144a409ceb3d72f27e51
MrDallOca <tdalloca@gmail.com>
2018-06-06 21:58:22 -0300

    lab17

---
+++ aluno.py
@@ -0,0 +1,7 @@
+class Aluno:
+    def __init__(self, nome, ra, email, usuario, senha):
+        self.nome = nome
+        self.ra = ra
+        self.email = email
+        self.usuario = usuario
+        self.senha = senha
